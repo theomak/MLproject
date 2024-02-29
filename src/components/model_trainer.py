87d926270dc72e2aa478d0e9b92eb8644132bacf
Theodoros Makridakis <thodorismak@gmail.com>
2024-02-29 15:36:31 +0200

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -24,7 +24,7 @@ from src.utils import save_object, evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifcats", "model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
 
 class ModelTrainer:
     def __init__(self):
